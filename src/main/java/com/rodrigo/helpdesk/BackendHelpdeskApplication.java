e2aec1fde54dd5df223198a3c3ab78616a27b99a
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2022-02-14 21:55:36 -0300

    helpdeskBackend

---
+++ src/main/java/com/rodrigo/helpdesk/BackendHelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHelpdeskApplication.class, args);
+	}
+
+}
